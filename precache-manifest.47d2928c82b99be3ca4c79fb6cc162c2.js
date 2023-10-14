e4e4ec17d9136c500e53fdcc4dcd39479f832f4e
poda850 <poda850@gmail.com>
2023-10-14 22:27:46 +0300

    Updates

---
+++ precache-manifest.47d2928c82b99be3ca4c79fb6cc162c2.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c5e635a3a50e96f2f7425348ca52e96",
+    "url": "/FreshCart-React-Project/index.html"
+  },
+  {
+    "revision": "a0882962cada45bd9092",
+    "url": "/FreshCart-React-Project/static/css/2.5503fa0a.chunk.css"
+  },
+  {
+    "revision": "27401da9845669c361f6",
+    "url": "/FreshCart-React-Project/static/css/main.7d3812e9.chunk.css"
+  },
+  {
+    "revision": "a0882962cada45bd9092",
+    "url": "/FreshCart-React-Project/static/js/2.0b44cf25.chunk.js"
+  },
+  {
+    "revision": "1f71dc1d3da316a113d78db3161bda15",
+    "url": "/FreshCart-React-Project/static/js/2.0b44cf25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27401da9845669c361f6",
+    "url": "/FreshCart-React-Project/static/js/main.980ad902.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/FreshCart-React-Project/static/js/main.980ad902.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41566d2b0698d26050b9",
+    "url": "/FreshCart-React-Project/static/js/runtime-main.1c7ed0e1.js"
+  },
+  {
+    "revision": "547465b7852370feb18f4a0249131be6",
+    "url": "/FreshCart-React-Project/static/media/App-store.547465b7.png"
+  },
+  {
+    "revision": "c5ab0dbccfda7068ed0607c5b554097a",
+    "url": "/FreshCart-React-Project/static/media/Google-play.c5ab0dbc.webp"
+  },
+  {
+    "revision": "5cce40c9243763538efa19a16b8a1969",
+    "url": "/FreshCart-React-Project/static/media/MasterCard_Logo.svg.5cce40c9.png"
+  },
+  {
+    "revision": "8479bfc1e373b92ec46b3629324fc340",
+    "url": "/FreshCart-React-Project/static/media/amazonPay.8479bfc1.png"
+  },
+  {
+    "revision": "92cbbbfe8d2e78e27d21391faa3365e5",
+    "url": "/FreshCart-React-Project/static/media/blog-img-1.92cbbbfe.jpeg"
+  },
+  {
+    "revision": "421de816d35ec581a5e2374d13ac4788",
+    "url": "/FreshCart-React-Project/static/media/blog-img-2.421de816.jpeg"
+  },
+  {
+    "revision": "fb191a80879759685a6a692976299c47",
+    "url": "/FreshCart-React-Project/static/media/error.fb191a80.svg"
+  },
+  {
+    "revision": "005c9aa92b564b73b7582cc4f1fa49cb",
+    "url": "/FreshCart-React-Project/static/media/fa-brands-400.005c9aa9.woff2"
+  },
+  {
+    "revision": "bb8cd014d7a55672934233c354e1c4a3",
+    "url": "/FreshCart-React-Project/static/media/fa-brands-400.bb8cd014.ttf"
+  },
+  {
+    "revision": "675809e48e35c47d51c7d6fcc687ee28",
+    "url": "/FreshCart-React-Project/static/media/fa-regular-400.675809e4.ttf"
+  },
+  {
+    "revision": "747442fa76f1d9a31f9a54a2e8a4b448",
+    "url": "/FreshCart-React-Project/static/media/fa-regular-400.747442fa.woff2"
+  },
+  {
+    "revision": "0248ab19e74fc3731de14d647db4687a",
+    "url": "/FreshCart-React-Project/static/media/fa-solid-900.0248ab19.ttf"
+  },
+  {
+    "revision": "d5e647388e2415268b700d3df2e30a0d",
+    "url": "/FreshCart-React-Project/static/media/fa-solid-900.d5e64738.woff2"
+  },
+  {
+    "revision": "58f1253e5639d508a6fe310c97327f82",
+    "url": "/FreshCart-React-Project/static/media/fa-v4compatibility.58f1253e.ttf"
+  },
+  {
+    "revision": "f20773a753bf1fbe9ccec04f90a470ea",
+    "url": "/FreshCart-React-Project/static/media/fa-v4compatibility.f20773a7.woff2"
+  },
+  {
+    "revision": "01e44e6a6ea33dd28fc38ab9b9a3cd53",
+    "url": "/FreshCart-React-Project/static/media/freshcart-logo.01e44e6a.svg"
+  },
+  {
+    "revision": "aaca9bd21f9c089995739b9ae1ceae1b",
+    "url": "/FreshCart-React-Project/static/media/png-transparent-paypal-logo-text-line-blue.aaca9bd2.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/FreshCart-React-Project/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/FreshCart-React-Project/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/FreshCart-React-Project/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/FreshCart-React-Project/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "ec23b97c3c3c0517326378f627bf4053",
+    "url": "/FreshCart-React-Project/static/media/slider-image-1.ec23b97c.jpeg"
+  },
+  {
+    "revision": "07e637f30b61063c855d506580e1f00b",
+    "url": "/FreshCart-React-Project/static/media/slider-image-2.07e637f3.jpeg"
+  },
+  {
+    "revision": "0642acda0b75f2fb74f9f44f264ae631",
+    "url": "/FreshCart-React-Project/static/media/slider-image-3.0642acda.jpeg"
+  }
+]);
